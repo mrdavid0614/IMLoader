abdf1d42347c43bbfb628dd914d35893f093b716
Raymond Mora <r.mora@gbh.com.do>
2022-05-16 22:36:16 -0400

    Create a spinner component

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Spinner() {
+  return (
+    <div className={`rounded-full border-2 border-l-[3px] border-gray-400 border-l-[#2F80ED] h-[80px] w-[80px] animate-spin`}></div>
+  )
+}
+
+export default Spinner
